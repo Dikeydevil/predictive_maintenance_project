c7b4a074711525ae57037bdd3778c7b0e723ef19
Kirill <your_email@whatever.com>
2025-03-28 23:17:47 +0300

    Добавлена requirements.txt analysis_and_model.py

---
+++ analysis_and_model.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import (
+    accuracy_score,
+    confusion_matrix,
+    classification_report,
+    roc_auc_score,
+    roc_curve
+)
+
+
---
+++ presentation.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import reveal_slides as rs
+
+def presentation_page():
+    st.title("📽️ Презентация проекта")
+
+
